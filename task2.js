43e97c76c9558c1ef10f4e90024163d9781abb00
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-02-04 12:08:20 +0300

    feat: task2 is completed

---
+++ task2.js
@@ -0,0 +1,8 @@
+//Выполнение данной задачи заняло 7 минут + рефакторинг
+
+
+const roundToFive = (number) => result =  Math.round(number / 5) * 5;
+
+console.log(roundToFive(27))
+console.log(roundToFive(27.8))
+console.log(roundToFive(41.7))
